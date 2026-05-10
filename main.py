1d5b2ea9dc30d79318ab4feb8078f68aedac36e8
LexaKurilovi4 <117000274+LexaKurilovi4@users.noreply.github.com>
2026-05-10 22:45:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import sys
+import warnings
 from PyQt6.QtWidgets import QApplication
 from src.gui import App
 
-def main() -> None:
-    app = QApplication(sys.argv)
-    window = App()
-    window.show()
-    sys.exit(app.exec())
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-if __name__ == "__main__":
-    main()
+app = QApplication(sys.argv)
+window = App()
+window.show()
+sys.exit(app.exec())
